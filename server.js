9177abf2d72867524aaed904e289878aadc33bc0
tomereiss <tomer.re@intentiq.com>
2025-05-27 23:57:07 +0300

    Fix production configuration

--- server.js
+++ server.js
@@ -105,6 +105,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
